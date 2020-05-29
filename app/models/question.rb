fcf0f438f2273d4e60ea87e5b9312ec9b33ef52f
xaero-w <d_shalimov@mail.ru>
2020-05-29 18:40:15 +0300

    add validation in model: user (3); question (1)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ApplicationRecord
+
+  belongs_to :user
+
+  validates :user, :text, presence: true
+     
+  #===== поверка максимальной длины текста вопроса (максимум 255 символов)
+  validates :text, length: { maximum: 255 }
+
 end
