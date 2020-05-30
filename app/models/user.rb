0641883c0c0e9ed0deec615bc2656c366cbbeec7
xaero-w <d_shalimov@mail.ru>
2020-05-30 17:54:49 +0300

    edit model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,71 @@
 require 'openssl'
 
-class User < ApplicationRecord
+require 'openssl'
+
+# Модель пользователя.
+#
+# Каждый экземпляр этого класса — загруженная из БД инфа о конкретном юзере.
+class User < ActiveRecord::Base
+  # Параметры работы для модуля шифрования паролей
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
+  # Виртуальное поле, которое не сохраняется в базу. Из него перед сохранением
+  # читается пароль, и сохраняется в базу уже зашифрованная версия пароля в
+  # реальные поля password_salt и password_hash.
+  attr_accessor :password
+
+  # Эта команда добавляет связь с моделью Question на уровне объектов она же
+  # добавляет метод .questions к данному объекту.
+  #
+  # Вспоминайте уроки про рельционные БД и связи между таблицами.
+  #
+  # Когда мы вызываем метод questions у экземпляра класса User, рельсы
+  # поймут это как просьбу найти в базе все объекты класса Questions со
+  # значением user_id равный user.id.
+  has_many :questions
+
+  #===== проверка формата почты
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+
+  #===== проверка максимальной длины юзернейма пользователя (не больше 40 символов)
+  validates :username, length: { maximum: 40 }
+
+  #===== проверка формата юзернейма пользователя (только латинские буквы, цифры, и знак _)
+  FORMAT_USERNAME = /\A[a-z]+\_\z/
+  validates :username, format: { with: FORMAT_USERNAME }
+
+
+  # Валидация, которая проверяет, что поля email и username не пустые и не равны
+  # nil. Если не задан email и username, объект не будет сохранен в базу.
+  validates :email, :username, presence: true
+
+  # Валидация, которая проверяет уникальность полей email и username. Если в
+  # базе данных уже есть записи с такими email и/или username, объект не будет
+  # сохранен в базу.
+  validates :email, :username, uniqueness: true
+
+  # Поле password нужно только при создании (create) нового юзера — регистрации.
+  # При аутентификации (логине) мы будем сравнивать уже зашифрованные поля.
+  validates :password, presence: true, on: :create
+
+  # Валидация, которая проверяет совпадения значений полей password и
+  # password_confirmation. Понадобится при создании формы регистрации, чтобы
+  # снизить число ошибочно введенных паролей.
+  validates_confirmation_of :password
+
+  # Ошибки валидаций можно посмотреть методом errors.
+
+  # Перед сохранением объекта в базу, создаем зашифрованный пароль, который
+  # будет хранится в БД.
+  before_save :encrypt_password
+
+  # Шифруем пароль, если он задан
   def encrypt_password
     if password.present?
-      # Создаем т.н. «соль» — случайная строка, усложняющая задачу хакерам по
-      # взлому пароля, даже если у них окажется наша БД.
-      #У каждого юзера своя «соль», это значит, что если подобрать перебором пароль
-      # одного юзера, нельзя разгадать, по какому принципу
-      # зашифрованы пароли остальных пользователей
+      # Создаем т. н. «соль» — рандомная строка усложняющая задачу хакерам по
+      # взлому пароля, даже если у них окажется наша база данных.
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
       # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
@@ -22,24 +77,24 @@ class User < ApplicationRecord
         )
       )
 
-      # Оба поля попадут в базу при сохранении (save).
+      # Оба поля окажутся записанными в базу при сохранении (save).
     end
   end
 
-  # Служебный метод, преобразующий бинарную строку в шестнадцатиричный формат,
+  # Служебный метод, преобразующий бинарную строку в 16-ричный формат,
   # для удобства хранения.
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
 
   # Основной метод для аутентификации юзера (логина). Проверяет email и пароль,
-  # если пользователь с такой комбинацией есть в базе, возвращает этого
-  # пользователя. Если нет — возвращает nil.
+  # если пользователь с такой комбинацией есть в базе возвращает этого
+  # пользователя. Если нету — возвращает nil.
   def self.authenticate(email, password)
     # Сперва находим кандидата по email
     user = find_by(email: email)
 
-    # Если пользователь не найден, возвращает nil
+    # Если пользователь не найдет, возвращаем nil
     return nil unless user.present?
 
     # Формируем хэш пароля из того, что передали в метод
@@ -57,28 +112,4 @@ class User < ApplicationRecord
     # Иначе, возвращаем nil
     nil
   end
-
-  has_many :questions
-
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-
-  #===== проверка формата почты
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: EMAIL_REGEX }
-
-  #===== проверка максимальной длины юзернейма пользователя (не больше 40 символов)
-  validates :username, length: { maximum: 40 }
-
-  #===== проверка формата юзернейма пользователя (только латинские буквы, цифры, и знак _)
-  FORMAT_USERNAME = /\A[a-z]+\_\z/
-  validates :username, format: { with: FORMAT_USERNAME }
-
-  attr_accessor :password
-
-  # валидация новых пользователей при создании
-  validates :password, presence: true, on: :create
-
-  # и поле подтверждения пароля
-  validates_confirmation_of :password
 end
