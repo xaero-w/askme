becb7bbf1fe54c21207f8a185c46c30e40e620ef
xaero-w <d_shalimov@mail.ru>
2020-06-04 12:36:27 +0300

    synchronize, last commit is revelant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(
       name: "Dmitry",
       username: "xaero",
-      avatar_url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2a143cd-ef7d-44b1-acea-4bfca8f719af/d8ewyyb-6748015c-f635-4a3b-b908-2823ff77645b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTJhMTQzY2QtZWY3ZC00NGIxLWFjZWEtNGJmY2E4ZjcxOWFmXC9kOGV3eXliLTY3NDgwMTVjLWY2MzUtNGEzYi1iOTA4LTI4MjNmZjc3NjQ1Yi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.NicR9NUmB6iqTlFsV_e1QzGijN1hDN25BYxPjEI8YhU"
+      avatar_url: "https://i.pinimg.com/originals/2f/0c/fa/2f0cfa9cf4cf82c8a97c3576d2024684.jpg"
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   # get 'users/new'
   # get 'users/edit'
   # get 'users/show'
+  # get 'users/index'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#show"
+  # root "users#index"
 
   resource :users
 
