046037d34b184bbf707c53f6066adfbf2914b364
xaero-w <d_shalimov@mail.ru>
2020-06-06 13:25:10 +0300

    edit models user - regular for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
   FORMAT_EMAIL = /\A.+@.+\z/
-  LETTERS_FOR_NAME = /[a-z0-9_-]/
+  LETTERS_FOR_NAME = /^[A-z0-9_-]+$/
 
   attr_accessor :password
   has_many :questions
