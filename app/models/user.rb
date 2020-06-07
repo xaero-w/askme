e6f3c7bc93298d739f58c007915c025c465ddf56
xaero-w <d_shalimov@mail.ru>
2020-06-07 17:44:23 +0300

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,43 +6,19 @@ class User < ApplicationRecord
   FORMAT_EMAIL = /\A.+@.+\z/
   LETTERS_FOR_NAME = /\A[a-zA-Z0-9_-]+\z/
 
-  # Все коллбэки
-
-  # Вызываем специальный метод и в виде символа передаём имя метода (его пишем сами),
-  # который будет вызван перед каким-либо действием
-
-  before_validation :before_validation
-  after_validation :after_validation
-
-  before_save :before_save
-  after_save :after_save
-
-  before_create :before_create
-  after_create :after_create
-
-  before_update :before_update
-  after_update :after_update
-
-  before_destroy :before_destroy
-  after_destroy :after_destroy
-
   attr_accessor :password
   has_many :questions
 
   #=========== Проверка формата ввода юзернейма, уникальности и его длинны.
-  validates :username, length: { maximum: 40 },
-                       presence: true,
-                       format: { with: /\A[a-zA-Z0-9_-]+\z/ },
-                       uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :username, length: { maximum: 40 }, format: { with: /\A[a-zA-Z0-9_-]+\z/ }
 
   #=========== Проверка уникальности почтового адреса.
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: { with: /\A.+@.+\z/ }
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A.+@.+\z/ }
 
   #=========== Проверка наличия пароля.
-  validates :password, presence: true,
-                       on: :create
+  validates :password, presence: true, on: :create
 
   validates_confirmation_of :password
 
@@ -51,11 +27,15 @@ class User < ApplicationRecord
   before_validation :lower_case_email
 
   def lower_case_name
-    self.username = username.downcase
+    if self.username.present?
+      self.username = username.downcase
+    end
   end
 
   def lower_case_email
-    self.email = email.downcase
+    if self.email.present?
+      self.email = email.downcase
+    end
   end
 
   def self.hash_to_string(password_hash)
@@ -82,19 +62,6 @@ class User < ApplicationRecord
           password, password_salt, ITERATIONS, DIGEST.length, DIGEST
         )
       )
-    end
-  end
-
-  private
-
-  # Этот код нужен только для демонстрации валидаций, потом мы его удалим.
-  # Код создаёт все возможные пары методов before_validation, after_validation, before_save и так далее.
-
-  %w(validation save create update destroy).each do |action|
-    %w(before after).each do |time|
-      define_method("#{time}_#{action}") do
-        puts "******> #{time} #{action}"
-      end
     end
   end
 end
