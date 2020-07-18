ced7196fb0f44af4c28fb78b32382997c3b912bc
xaero-w <d_shalimov@mail.ru>
2020-07-18 12:46:29 +0300

    add time zone in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Askme
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'UTC'
+
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+
+    config.i18n.fallbacks = [:en]
   end
 end
