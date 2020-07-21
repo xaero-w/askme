45213029ad2338d6150a94d696989ab11759f19c
xaero-w <d_shalimov@mail.ru>
2020-07-21 22:05:56 +0300

    edit locales

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Askme
     config.time_zone = 'UTC'
 
     config.i18n.default_locale = :ru
-    config.i18n.locale = :ru
+    config.i18n.available_locales = %i[ru en]
 
     config.i18n.fallbacks = [:en]
   end
