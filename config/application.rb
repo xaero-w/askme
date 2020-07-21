4aee5cea313b2e839fc4aaaca820af4950043ab7
xaero-w <d_shalimov@mail.ru>
2020-07-21 22:17:37 +0300

    edit locales

--- config/application.rb
+++ config/application.rb
@@ -30,11 +30,12 @@ module Askme
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
-    config.time_zone = 'UTC'
+    config.active_record.raise_in_transactional_callbacks = true
 
-    config.i18n.default_locale = :ru
-    config.i18n.available_locales = %i[ru en]
+    config.time_zone = 'Moscow'
+
+    config.i18n.default_locale = :en
+    config.i18n.locale = :ru
 
     config.i18n.fallbacks = [:en]
   end
