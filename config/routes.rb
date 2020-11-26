efcab9fe3e938c6c674dd8727677a3b30577e3f1
xaero-w <d_shalimov@mail.ru>
2020-11-26 17:03:52 +0300

    security fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users
+  resources :users, except: [:destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-  resources :sessions, only: [:new, :create, :destroy]
 
-  # Синонимы путей — в дополнение к созданным в ресурсах выше.
-  #
-  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
-  # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
